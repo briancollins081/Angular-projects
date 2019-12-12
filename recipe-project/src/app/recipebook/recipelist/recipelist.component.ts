850e448834fa03d2e432af40e7f6c7994c5b41d9
briancollins081 <briancollins081@gmail.com>
2019-12-12 16:06:11 +0300

    Adding content to recipe components

--- recipe-project/src/app/recipebook/recipelist/recipelist.component.ts
+++ recipe-project/src/app/recipebook/recipelist/recipelist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipelist',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipelist.component.css']
 })
 export class RecipelistComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      "Abc Test Recipe", 
+      "Grilled Eggs mixed with chillie, made for all ages at your desired taste", 
+      "https://get.pxhere.com/photo/restaurant-dish-meal-food-vegetable-recipe-meat-cuisine-asian-food-garnish-dinner-tasty-vegetarian-food-gastronomy-mediterranean-food-fried-food-animal-source-foods-middle-eastern-food-adana-kebab-mixed-grill-fat-ny-ros-1376098.jpg"
+    )
+  ];
   constructor() { }
 
   ngOnInit() {
