bdc8fc4853e31ee9241b20a5e07d70a6ca95e4e3
briancollins081 <briancollins081@gmail.com>
2019-12-19 16:38:45 +0300

    Recap on ngIf and ngFor

--- numbers-app/src/app/app.component.ts
+++ numbers-app/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'numbers-app';
+  oddNumbers: number[] = [1,3,5,7,9];
+  evenNumbers: number[] = [2,4,6,8]
+  onlyOdd: boolean = false;
 }
