96ee2b0a91c038868e45fb103ef65deb31273c0d
briancollins081 <briancollins081@gmail.com>
2019-12-19 11:18:07 +0300

    wip

--- recipe-project/src/app/recipebook/recipelist/recipelist.component.ts
+++ recipe-project/src/app/recipebook/recipelist/recipelist.component.ts
@@ -14,7 +14,7 @@ export class RecipelistComponent implements OnInit {
       "https://get.pxhere.com/photo/restaurant-dish-meal-food-vegetable-recipe-meat-cuisine-asian-food-garnish-dinner-tasty-vegetarian-food-gastronomy-mediterranean-food-fried-food-animal-source-foods-middle-eastern-food-adana-kebab-mixed-grill-fat-ny-ros-1376098.jpg"
     ),
     new Recipe(
-      "Abc Test Recipe", 
+      "Grilled Eggs Recipe", 
       "Grilled Eggs mixed with spices at your desired taste", 
       "https://img.taste.com.au/OBKIMrub/w643-h428-cfill-q90/taste/2018/05/creamy-chicken-piccata-pasta-137359-1.jpg"
     )
