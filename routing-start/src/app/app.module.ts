8d7b7fb2fcac49679226601da9c9aac98e4319b1
briancollins081 <briancollins081@gmail.com>
2020-01-13 15:51:19 +0300

    Using redirection and wildcard ** to redirect
    Note: The ** route should always come last

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -13,14 +13,14 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent, runGuardsAndResolvers: 'pathParamsChange'  }
-    ],
-    runGuardsAndResolvers: "always"
+      { path: ':id/:name', component: UserComponent }
+    ]
   },
   {
     path: 'servers', component: ServersComponent, children: [
@@ -28,6 +28,8 @@ const appRoutes: Routes = [
       { path: ':id/edit', component: EditServerComponent }
     ]
   },
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({
@@ -38,7 +40,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
