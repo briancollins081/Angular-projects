54767ccd3f7e40d787d1eef4a60bafccba72cca7
briancollins081 <briancollins081@gmail.com>
2020-01-13 12:53:20 +0300

    Passing query parameters and fragments via the routerLink and programmatic

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
-
+  paramsSubscription: Subscription;
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
@@ -17,12 +18,15 @@ export class UserComponent implements OnInit {
       name: this.activatedRoute.snapshot.params['name']
     }
     //subcribes to the params parameters
-    this.activatedRoute.params.subscribe(
-      (params)=>{
+    this.paramsSubscription = this.activatedRoute.params.subscribe(
+      (params:Params)=>{
         this.user.name = params['name'];
         this.user.id = params['id'];
       }
     );
+  }
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe();
   }
 
 }
