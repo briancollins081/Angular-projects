2360b6687d84e92eeaf570a707df3c0bd49b8574
briancollins081 <briancollins081@gmail.com>
2020-01-16 13:15:08 +0300

    Custom observable - using rxjs Subjects are mainly used to subscribe to cross component events that are defined in a service to pass data between two or more components Use event emmiters when using the @Output emitters within components

--- observables/src/app/app.component.ts
+++ observables/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   isActive: boolean;
+  userActivateSubscription: Subscription;
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.userService.activatedUserEmmiter.subscribe(activeState=>{
-      this.isActive=activeState;
+    this.userActivateSubscription = this.userService.activatedUserEmmiter.subscribe(activeState => {
+      this.isActive = activeState;
     })
-   }
+  }
+
+  ngOnDestroy(){
+    this.userActivateSubscription.unsubscribe();
+  }
 }
