24cc80106bfeb70593a0386b0bf534e9247d112e
briancollins081 <briancollins081@gmail.com>
2020-01-16 19:06:08 +0300

    Forms Module - Template Driven Forms -- Validation ---Outputting validation error messages

--- handling-angular-forms/src/app/app.component.ts
+++ handling-angular-forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,16 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: true}) form:NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(frm:NgForm){
+  /* onSubmit(frm:NgForm){
     console.log(frm); 
+  } */
+
+  onSubmit(){
+    console.log(this.form);
   }
 }
