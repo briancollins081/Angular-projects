0cd2f5830f4c3270fd8e30e6096b67628a8868b1
briancollins081 <briancollins081@gmail.com>
2020-01-16 19:32:08 +0300

    Forms Module - Template Driven Forms -- Set default values with ngModel two way property binding

--- handling-angular-forms/src/app/app.component.ts
+++ handling-angular-forms/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f', {static: true}) form:NgForm;
   defaultQuestion="teacher";
+  secretAns:string='';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
