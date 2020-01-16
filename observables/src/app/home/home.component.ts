0776d007cb5dc4c963138dbb01c9d78141531551
briancollins081 <briancollins081@gmail.com>
2020-01-16 12:03:02 +0300

    Custom observable - capturing data changes

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy {
   countObservableSubscription: Subscription;
+  customCntObservable: Subscription;
   constructor() { }
 
   ngOnInit() {
-    this.countObservableSubscription = interval(1000).subscribe((c => {
+    /* this.countObservableSubscription = interval(1000).subscribe((c => {
       console.log(c);
-    }));
+    })); */
+    const customCountObservable = Observable.create(observer => {
+      let c = 0;
+      setInterval(() => {
+        observer.next(c);
+        c++
+      }, 1000)
+
+    });
+
+    this.customCntObservable = customCountObservable.subscribe((count) => {
+      console.log(count);
+    })
   }
 
-  ngOnDestroy(){
-    this.countObservableSubscription.unsubscribe();
+  ngOnDestroy() {
+    // this.countObservableSubscription.unsubscribe();
+    this.customCntObservable.unsubscribe();
   }
 
 }
