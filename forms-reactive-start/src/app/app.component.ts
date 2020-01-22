33c444dfc7bbe21630ad5483f58fc82e2c8e16b2
briancollins081 <briancollins081@gmail.com>
2020-01-22 16:32:17 +0300

    Reactive Forms - Using custom async validator

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,13 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
-  forbiddenUserNames = ['Brian','Collins'];
+  forbiddenUserNames = ['Brian', 'Collins'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       'gender': new FormControl('female'),
       'hobbies': new FormArray([])
@@ -32,11 +33,24 @@ export class AppComponent implements OnInit {
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
-  forbiddenNames(control: FormControl):{[s:string]:boolean}{
-    if(this.forbiddenUserNames.indexOf(control.value) != -1){
-      return {'nameIsForbidden': true};
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) != -1) {
+      return { 'nameIsForbidden': true };
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject)=> {
+      setTimeout(()=>{
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbidden': true});
+        }else{
+          resolve(null);
+        }
+      }, 1550);
+    });
+    return promise;
   }
 
 }
