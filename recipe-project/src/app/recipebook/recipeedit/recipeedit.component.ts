ea39786b3e175f102479821bd8bcfbcf2bb8d1a4
Brian Andere <briancollins081@gmail.com>
2020-01-30 16:16:32 +0300

    Recipe Project - Forms - Template for reactive forms - creating form and synching it with html

--- recipe-project/src/app/recipebook/recipeedit/recipeedit.component.ts
+++ recipe-project/src/app/recipebook/recipeedit/recipeedit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeBookService } from '../recipebook.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipeedit',
@@ -9,8 +12,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeeditComponent implements OnInit {
   id: number;
   editMode: boolean = false;
+  recipeForm: FormGroup;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private recipeBookService: RecipeBookService) { }
 
   ngOnInit() {
     this.activatedRoute
@@ -18,8 +23,30 @@ export class RecipeeditComponent implements OnInit {
       .subscribe((params:Params)=>{
         this.id = +params['id'];
         this.editMode = (params['id'] != null);
-        console.log(this.editMode);
+        // console.log(this.editMode);
+        this.initForm();
       })
+  }
+
+  onSubmit(){
+    console.log(this.recipeForm);
+  }
+
+  initForm(){
+    let recipe = this.recipeBookService.getRecipe(this.id);
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    if(this.editMode){
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription)
+    });
   }
 
 }
